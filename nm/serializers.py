47c479f920155f99a28d3b600fcd32c993a72e8a
Matt Weeden <matt.weeden@myhealthaccess.net>
2019-07-16 08:33:41 -0500

    Update with serializer

--- nm/serializers.py
+++ nm/serializers.py
@@ -1,14 +1,8 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from nm.models import Game
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+class GameSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created = serializers.DateTimeField()
+    updated = serializers.DateTimeField()
+    cells = models.CharField(max_length=36)
