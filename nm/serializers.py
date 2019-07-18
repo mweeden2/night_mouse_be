210b4c60c9557910238e0922fb6003638e5e550e
Matt Weeden <matt.weeden@myhealthaccess.net>
2019-07-18 08:34:41 -0500

    Flesh out first serializer

--- nm/serializers.py
+++ nm/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from nm.models import Game
 
+from datetime import datetime
+
 class GameSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    created = serializers.DateTimeField()
+    created = serializers.DateTimeField(default=datetime.now())
     updated = serializers.DateTimeField()
-    cells = models.CharField(max_length=36)
+    cells = serializers.CharField(max_length=36)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Game` instance, given the validated data.
+        """
+        return Game.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Game` instance, given the validated data.
+        """
+        instance.created = validated_data.get('created', instance.created)
+        instance.updated = validated_data.get('updated', instance.updated)
+        instance.cells = validated_data.get('cells', instance.cells)
+        instance.save()
+        return instance
